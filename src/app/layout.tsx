7db58a672972099f07616050711fb8038631f5dc
dhantosy <dhantosy@gmail.com>
2023-10-23 13:54:24 +0800

    feat: added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
@@ -16,6 +17,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-8CKH38ZP4C" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-8CKH38ZP4C');
+        `}
+        </Script>
+
+      </head>
       <body className={montserrat.className}>{children}</body>
     </html>
   )
