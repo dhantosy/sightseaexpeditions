61ec4cfdd126c3e7547c60fa697e25b3216d6618
dhanto santika <dhantosy@gmail.com>
2024-06-26 20:36:09 +0800

    feat: improved ux

--- src/components/ui/CardHorizontalItem/index.tsx
+++ src/components/ui/CardHorizontalItem/index.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { FaLocationDot, FaCalendar, FaRegEye } from 'react-icons/fa6';
-import { MdScubaDiving } from "react-icons/md";
+import { MdScubaDiving } from 'react-icons/md';
+import { Button } from '@/components/ui/Button'
 import { CardHorizontalItemProp } from './types'
 
 export default function CardHorizontalItem({
@@ -20,7 +21,7 @@ export default function CardHorizontalItem({
     <Link href={url} className='relative block top-0 overflow-hidden rounded-2xl transition-all hover:-top-1 group'>
       <div className='flex flex-wrap border border-slate-200 rounded-2xl p-3'>
         <div className='relative basis-[100%] md:basis-[40%] grow-0 rounded-2xl shadow-xl overflow-hidden'>
-          <div className='relative w-full pb-[60%] md:pb-[85%]'>
+          <div className='relative w-full pb-[60%] md:pb-[85%] 2lg:pb-[125%] 3lg:pb-[85%]'>
             <Image
               src={img}
               alt={imgAlt}
@@ -52,10 +53,17 @@ export default function CardHorizontalItem({
             <FaCalendar />
             <span>{date}</span>
           </div>
-          <div className='mt-6 md:mt-3 md:absolute bottom-3'>
-            <span className='text-sm'>From </span>
-            <span className='text-lg font-bold'>{price} </span>
-            <span className='text-sm'>/ pax</span>
+          <div className='mt-6 md:mt-3 2lg:mt-9 md:absolute 2lg:relative 3lg:absolute bottom-3 left-0 flex justify-between w-full pl-0 md:pl-7 2lg:pl-0 3lg:pl-7 pr-0 md:pr-3 gap-3 flex-col 2xs:flex-row'>
+            <div>
+              <span className='text-sm'>From </span>
+              <span className='text-lg font-bold'>{price} </span>
+              <span className='text-sm'>/ pax</span>
+            </div>
+            <div>
+              <Button variant='secondary' size='sm'>
+                Book Now
+              </Button>
+            </div>
           </div>
         </div>
       </div>
