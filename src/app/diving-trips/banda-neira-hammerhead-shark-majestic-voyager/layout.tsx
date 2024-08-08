1fd2b37dc09274ef16fb120f4f5b652e2b53f2b8
dhanto santika <dhantosy@gmail.com>
2024-08-08 11:42:19 +0800

    fix: fixed title event

--- src/app/diving-trips/banda-neira-hammerhead-shark-majestic-voyager/layout.tsx
+++ src/app/diving-trips/banda-neira-hammerhead-shark-majestic-voyager/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Banda Neira Hammerhead Shark Dive Trip with Majestic Voyager - Sightsea Expeditions',
+  title: 'Banda Neira Dive Trip with Majestic Voyager - Sightsea Expeditions',
   description: 'Embark on an extraordinary underwater journey with our curated collection of luxury dive trips. Explore pristine dive sites, encounter incredible marine life, and indulge in unparalleled comfort aboard our hand-picked liveaboard vessels. Discover your perfect dive vacation today.',
   keywords: 'luxury dive trips, liveaboard diving, dive vacations, curated dive experiences, dive destinations, diving holidays, premium dive trips, exclusive dive travel, dive trip planner, best dive trips, diving adventures, underwater exploration',
   openGraph: {
-    title: 'Banda Neira Hammerhead Shark Dive Trip with Majestic Voyager - Sightsea Expeditions',
+    title: 'Banda Neira Dive Trip with Majestic Voyager - Sightsea Expeditions',
     description: 'Embark on an extraordinary underwater journey with our curated collection of luxury dive trips. Explore pristine dive sites, encounter incredible marine life, and indulge in unparalleled comfort aboard our hand-picked liveaboard vessels. Discover your perfect dive vacation today.',
     type: 'website',
     images: '/assets/images/banner-photo.jpg'
