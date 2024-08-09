90c318741ebc7baea5cd4c89d9e7233a77e022f4
dhanto santika <dhantosy@gmail.com>
2024-08-09 11:22:19 +0800

    feat: added next sitemap generator

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: 'https://www.sightseaexpeditions.com/',
+  generateRobotsTxt: true, 
+  changefreq: 'monthly',
+  generateIndexSitemap: false,
+  exclude: ['/manifest.webmanifest'],
+}
